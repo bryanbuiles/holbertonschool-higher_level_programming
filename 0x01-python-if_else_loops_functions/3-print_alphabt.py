66e10c4983837751d60819978a284a791f76cb5b
bryanbuiles <1482@holbertonschool.com>
2020-04-28 13:49:06 -0500

    task 3

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
 for a in range(97, 97 + 26):
-    if a == 113 or a == 101:
-        continue
-    print("{:c}".format(a), end='')
+    if a != 113 and a != 101:
+        print("{:c}".format(a), end='')
