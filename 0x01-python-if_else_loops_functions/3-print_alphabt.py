42d0ac226505556682b6020606d2a0e8ad4c501e
bryanbuiles <1482@holbertonschool.com>
2020-04-28 14:08:26 -0500

    task 3

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
-for char in range(ord('a'), ord('z')+1):
-    if chr(char) != 'q' and chr(char) != 'e':
-        print('{:c}'.format(char), end="")
+for a in range(97, 97 + 26):
+    if a == 113 or a == 101:
+        a = +1
+        continue
+    print("{:c}".format(a), end='')
