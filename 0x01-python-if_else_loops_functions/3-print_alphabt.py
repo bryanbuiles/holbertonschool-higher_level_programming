f66e97833bdc19d0f9fe07832ea521a9c67b2de2
bryanbuiles <60362838+bryanbuiles@users.noreply.github.com>
2020-04-28 13:57:46 -0500

    update

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
-for x in range(ord('a'), ord('z') + 1):
-    if x is not ord('e') and x is not ord('q'):
-        print("{:s}".format(chr(x)), end="")
+for a in range(97, 97 + 26):
+    if a == 113 or a == 101:
+        continue
+    print("{:c}".format(a), end='')
