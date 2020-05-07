e2e525ab519dc6d975a654157b9002ebc82b1265
bryanbuiles <1482@holbertonschool.com>
2020-05-07 16:26:36 -0500

    task 7

---
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def update_dictionary(a_dictionary, key, value):
+    key = str(key)
+    for i in a_dictionary.keys():
+        if i == key:
+            a_dictionary[key] = value
+        else:
+            a_dictionary.update({key: value})
+        return a_dictionary
