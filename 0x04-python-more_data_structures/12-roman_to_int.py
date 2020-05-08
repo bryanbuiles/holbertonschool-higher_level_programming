5ce5f39850fa0de5fa89fae5f60245e736c1c753
bryanbuiles <1482@holbertonschool.com>
2020-05-07 19:38:29 -0500

    task 12

---
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+def roman_to_int(roman_string):
+    from functools import reduce
+    li = []
+    if not roman_string:
+        return 0
+    if not isinstance(roman_string, str):
+        return 0
+    if not roman_string.isupper():
+        return 0
+    for i in roman_string:
+        li.append(i)
+    count = r = flag = 0
+    for k in li:
+        try:
+            if k == 'I' and (li[count + 1] == 'V' or li[count + 1] == 'X'):
+                r += 1
+                flag = 1
+        except IndexError:
+            pass
+        if k == 'I':
+            li[count] = 1
+        elif k == 'V':
+            li[count] = 5
+        elif k == 'X':
+            li[count] = 10
+        elif k == 'L':
+            li[count] = 50
+        elif k == 'C':
+            li[count] = 100
+        elif k == 'D':
+            li[count] = 500
+        elif k == 'M':
+            li[count] = 1000
+        count += 1
+    if flag == 1:
+        j = int(reduce(lambda x, y: x+y, li) - (2*r))
+    else:
+        j = int(reduce(lambda x, y: x+y, li))
+    return j
