491e294da18fb8c2022a830158a24a1bb2a4a333
bryanbuiles <1482@holbertonschool.com>
2020-05-07 21:08:13 -0500

    task 100

---
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def weight_average(my_list=[]):
+    if my_list:
+        list1 = list(t[0] * t[1] for t in my_list)
+        lis2 = list(t[1] for t in my_list)
+        print(lis2)
+        average = sum(list1) / sum(lis2)
+        return average
+    else:
+        return 0
