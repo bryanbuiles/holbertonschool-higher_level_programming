18c1b2bcdd78a899a8055eb0cb50963400455a1a
bryanbuiles <1482@holbertonschool.com>
2020-05-18 18:59:20 -0500

    task 101

--- 0x05-python-exceptions/101-safe_function.py
+++ 0x05-python-exceptions/101-safe_function.py
@@ -4,8 +4,8 @@ import sys
 
 def safe_function(fct, *args):
     try:
-        if len(args) < 3:
-            result = fct(args[0], args[1])
+        for arg in args:
+            result = fct(*args)
             return (result)
     except Exception as e:
         print("Exception: {}".format(e), file=sys.stderr)
