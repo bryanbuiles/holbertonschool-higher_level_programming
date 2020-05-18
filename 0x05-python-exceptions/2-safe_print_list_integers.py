8b29d55d761a0454f1f9900e73e97d97b6b83712
bryanbuiles <1482@holbertonschool.com>
2020-05-18 15:39:26 -0500

    task 2

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -1,16 +1,13 @@
 #!/usr/bin/python3
 def safe_print_list_integers(my_list=[], x=0):
-    count = 0
+    conta = 0
     try:
-        lista = []
-        for j in my_list:
-            if type(j) == int:
-                lista.append(j)
-        listat = list(lista[:x])
-        for i in listat:
-            print("{:d}".format(i), end="")
-            count += 1
+        for i in range(x):
+            if isinstance(my_list[i], int):
+                print("{:d}".format(my_list[i]), end="")
+                conta += 1
+        print("")
+        return conta
         print()
-        return(count)
-    except (ValueError, TypeError):
+    except (TypeError, ValueError):
         pass
