df81c3b6df7e1a65a7d07e043a7cf10fd36e4185
bryanbuiles <1482@holbertonschool.com>
2020-05-18 14:46:15 -0500

    task 1

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -1,12 +1,8 @@
 #!/usr/bin/python3
 def safe_print_integer(value):
     try:
-        stri = str(value)
-        if any(char.isdigit() for char in stri):
-            stri = int(stri)
-            print("{:d}".format(stri))
-            return (True)
-        else:
-            return (False)
-    except SyntaxError:
-        pass
+        value = int(value)
+        print("{:d}".format(value))
+        return (True)
+    except ValueError:
+        return (False)
