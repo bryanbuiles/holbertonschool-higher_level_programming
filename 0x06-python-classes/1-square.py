728952b60d5e29fbcdfba820cd2dcef947e6da6f
bryanbuiles <1482@holbertonschool.com>
2020-05-19 17:08:27 -0500

    task 1

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+'''an empty sqaure
+    '''
+
+
+class Square:
+    '''an empty sqaure
+    '''
+
+    def __init__(self, size):
+        ''' 
+        Args: 
+            size: size of square
+        '''
+        self.__size = size
