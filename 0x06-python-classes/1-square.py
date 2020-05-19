e3203db46d92b3de231c27e2b42f699cdfc7d265
bryanbuiles <1482@holbertonschool.com>
2020-05-19 17:24:37 -0500

    task 1

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,21 +1,18 @@
 #!/usr/bin/python3
-"""1-square.py
-
-	square class
+""" 1-square.py
+    Square class with private attribute size
 """
 
 
 class Square:
-    """1-square.py
-
-    square class
+    """Square Class
+    Class with private attribute size
     """
 
     def __init__(self, size):
-        """ __init__ Constructor
-
-        Initializa of class square
-        Args: 
-            size (intiger): size of square
+        """__init__ Contructor
+        Contructor of class Square
+        Args:
+            size (integer): privete attribute for zise Square
         """
         self.__size = size
