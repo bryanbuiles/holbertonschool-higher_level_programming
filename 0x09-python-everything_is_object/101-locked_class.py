24ba786aec50aa566c262cd17a87c5dfd07f430b
bryanbuiles <1482@holbertonschool.com>
2020-05-26 20:54:05 -0500

    task 31

---
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+    101-locked_class.py
+    class to lock a instance that create a new instance
+"""
+
+
+class LockedClass:
+    """
+        class to lock a instance
+    """
+    __slots__ = ["first_name"]
