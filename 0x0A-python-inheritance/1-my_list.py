31e3c8d7b9aa17ab2ccb03928edbe384a00d263f
bryanbuiles <1482@holbertonschool.com>
2020-06-02 18:05:26 -0500

    fix task 1

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -1,11 +1,14 @@
 #!/usr/bin/python3
+""" 1-my list
+"""
+
+
 class MyList(list):
     """Mylist
 
     Arguments:
         list (list) -- pattern class list
     """
-    pass
 
     def print_sorted(self):
         """ print soted list
