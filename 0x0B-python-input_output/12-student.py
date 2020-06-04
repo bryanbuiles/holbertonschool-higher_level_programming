326b161e4fefb8ca96773cba630e16ef331073ff
bryanbuiles <1482@holbertonschool.com>
2020-06-03 19:57:08 -0500

    task 12

---
+++ 0x0B-python-input_output/12-student.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+""" class student """
+
+
+class Student:
+    """ student data
+    """
+
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """ return jason dict """
+        if type(attrs) != list or attrs == None:
+            return self.__dict__
+        else:
+            dic = {}
+            for i in attrs:
+                if type(i) != str:
+                    return (self.__dict__)
+                if i in self.__dict__:
+                    dic[i] = self.__dict__[i]
+            return (dic)
