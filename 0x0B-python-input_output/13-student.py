8bfaf4caa3b88739f603bf9a963a8f92a607cfbb
bryanbuiles <1482@holbertonschool.com>
2020-06-03 21:23:02 -0500

    task 13

---
+++ 0x0B-python-input_output/13-student.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+""" class student """
+
+
+class Student:
+    """ student data
+    """
+
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """ return jason dict """
+        if type(attrs) != list or attrs is None:
+            return self.__dict__
+        else:
+            dic = {}
+            for i in attrs:
+                if type(i) != str:
+                    return (self.__dict__)
+                if i in self.__dict__:
+                    print(self.__dict__[i])
+                    dic[i] = self.__dict__[i]
+            return (dic)
+
+    def reload_from_json(self, json):
+        """ replace atrributes of student """
+        for i in json:
+            self.__dict__[i] = json[i]
