8a793fd2c63055b9efb6a780506c82a35e1d5c38
bryanbuiles <1482@holbertonschool.com>
2020-07-14 17:48:29 -0500

    task 0, 9,10

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,3 +1,4 @@
 -- My privilegies
-SHOW GRANTS user_0d_1@localhost;
-SHOW GRANTS user_0d_2@localhost;
+-- user_0d_1 and user_0d_2
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
+SHOW GRANTS FOR 'user_0d_2'@'localhost';
