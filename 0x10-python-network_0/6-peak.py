b5ff5dc4daba93056c2728a88d6b78b338417c27
bryanbuiles <1482@holbertonschool.com>
2020-08-18 18:05:34 -0500

    task 6

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -4,10 +4,6 @@
 
 def find_peak(list_of_integers):
     """ function to find the pick """
-    temp = 0
     if len(list_of_integers) == 0:
         return None
-    for i in list_of_integers:
-        if i > temp:
-            temp = i
-    return temp
+    return (max(list_of_integers))
