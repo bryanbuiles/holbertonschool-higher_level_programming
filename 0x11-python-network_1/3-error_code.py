6b1c94cebdb1e75a88899b28d707eeebd2dca513
bryanbuiles <1482@holbertonschool.com>
2020-08-24 13:41:33 -0500

    fix task 3

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -1,15 +1,14 @@
 #!/usr/bin/python3
 """ displays the error """
 import sys
-from urllib.request import Request, urlopen
-from urllib.error import HTTPError
+import urllib.request
+import urllib.error
 
 
 if __name__ == "__main__":
-    req = Request(sys.argv[1])
     try:
-        with urlopen(sys.argv[1]) as response:
+        with urllib.request.urlopen(sys.argv[1]) as response:
             html = response.read()
             print(html.decode('UTF-8'))
-    except HTTPError as e:
-        print('Error code: ', e.code)
+    except urllib.error.HTTPError as e:
+        print("Error code: {}".format(e.code))
