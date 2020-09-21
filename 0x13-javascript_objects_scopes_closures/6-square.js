f2a2c7cf028afa186b6d2d51675896db2e86e343
bryanbuiles <1482@holbertonschool.com>
2020-09-21 16:40:26 -0500

    fix task 6

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,41 +1,6 @@
 #!/usr/bin/node
 
-class Rectangle {
-  constructor (w, h) {
-    if (isNaN(w) || isNaN(h)) {
-    } else if (w <= 0 || h <= 0) {
-    } else {
-      this.width = w;
-      this.height = h;
-    }
-  }
-
-  print () {
-    for (let i = 0; i < this.height; i++) {
-      const array = [];
-      for (let j = 0; j < this.width; j++) {
-        array.push('X');
-      }
-      console.log(array.join(''));
-    }
-  }
-
-  rotate () {
-    const temp = this.width;
-    this.width = this.height;
-    this.height = temp;
-  }
-
-  double () {
-    this.width = this.width * 2;
-    this.height = this.height * 2;
-  }
-}
-class Square extends Rectangle {
-  constructor (size) {
-    super(size, size);
-  }
-
+module.exports = class Square extends require('./5-square') {
   charPrint (c) {
     if (c === undefined) {
       c = 'X';
@@ -48,7 +13,4 @@ class Square extends Rectangle {
       console.log(array.join(''));
     }
   }
-}
-
-module.exports = Rectangle;
-module.exports = Square;
+};
