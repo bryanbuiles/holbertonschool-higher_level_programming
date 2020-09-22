3266a59456f0f6485d5eac0fd94a0e87c1049aaf
bryanbuiles <1482@holbertonschool.com>
2020-09-22 18:09:57 -0500

    fix task 6

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -8,21 +8,14 @@ require('request').get(myArgs[2], function (err, r, body) {
   } else {
     const dicti = {};
     let count = 1;
-    const lista = [];
     const json = JSON.parse(body);
-    for (const items of json) {
-      if (items.completed === true) {
-        lista.push(items);
-      }
-    }
-    for (let key = 0; key < lista.length; key++) {
-      if (key !== 0) {
-        if (lista[key - 1].userId !== lista[key].userId) {
+
+    for (let key = 0; key < json.length; key++) {
+      if (json[key].completed === true) {
+        if (!(json[key].userId in dicti)) {
           count = 1;
-        } else {
-          count++;
         }
-        dicti[lista[key].userId] = count;
+        dicti[json[key].userId] = count++;
       }
     }
     console.log(dicti);
