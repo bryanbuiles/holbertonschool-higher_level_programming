1fb815152b4c8d5123d3aa5a2a4b4dff4968e587
bryanbuiles <1482@holbertonschool.com>
2020-09-22 16:28:33 -0500

    fix task 0 and 4

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -3,9 +3,9 @@
 const myArgs = process.argv;
 
 const fs = require('fs');
-fs.readFile(myArgs[2], 'utf8', (err, data) => {
+fs.readFile(myArgs[2], 'utf8', function (err, data) {
   if (err) {
-    console.error(err);
+    console.log(err);
   } else {
     console.log(data.toString().trim());
   }
--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -4,16 +4,11 @@ const myArgs = process.argv;
 const request = require('request');
 const options = {
   url: myArgs[2],
-  method: 'GET',
-  headers: {
-    Accept: 'application/json',
-    'Accept-Charset': 'utf-8'
-  }
+  method: 'GET'
 };
 request(options, function (err, res, body) {
   if (err) {
-    console.error(err);
-    return;
+    console.log(err);
   }
   const json = JSON.parse(body);
   const results = json.results;
@@ -22,6 +17,7 @@ request(options, function (err, res, body) {
     for (const cara of items.characters) {
       if (cara === 'https://swapi-api.hbtn.io/api/people/18/') {
         count++;
+        break;
       }
     }
   }
